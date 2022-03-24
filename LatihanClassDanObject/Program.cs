a8a6aa1ce94b70cac9a84fba0f2410a21d9584d7
ACER <ACER@LAPTOP-FQJJHO85>
2022-03-24 15:43:07 +0700

    Add project files.

---
+++ LatihanClassDanObject/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClassDanObject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat objek Taxi
+            Taxi taxi = new Taxi();
+
+            //pengesetan nilai properties
+            taxi.DriverName = " Jono ";
+            taxi.OnDuty = " True ";
+            taxi.NumPassenger = 10;
+
+            //pemanggilan method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ LatihanClassDanObject/Taxi.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClassDanObject
+{
+    internal class Taxi
+    {
+        // Properties
+        public string DriverName { get; set; }
+       public string OnDuty { get; set; }
+        public float NumPassenger { get; set; }
+
+        // Method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Nama : {0} ", DriverName);
+
+            Console.WriteLine("On Duty : {0} ", OnDuty);
+
+            Console.WriteLine("Number of Passenger : {0} ", NumPassenger);
+        }
+
+       public void PickUpPassenger()
+        {
+
+            Console.WriteLine("{0} sedang menjemput penumpang ", DriverName);
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang\n ", DriverName);
+        }
+    }
+}
